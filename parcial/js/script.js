57fb05a135bdf7f5f84f11353f7128cb3add9dc0
Jhon Édinson Salazar <69093836+jhonsl@users.noreply.github.com>
2024-11-05 23:12:48 -0500

    Add files via upload

--- parcial/js/script.js
+++ parcial/js/script.js
@@ -1,6 +1,10 @@
 let page = 1;
 let date = '2015-07-2';
 let image = document.getElementById('photo');
+let tableBody = document.getElementById('table-body');
+let idP = document.getElementById('id');
+let martianP = document.getElementById('martian');
+let earthP = document.getElementById('earth');
 
 function loadData() {
 	fetch(
@@ -11,8 +15,6 @@ function loadData() {
 }
 
 function setTableData(data) {
-	const tableBody = document.getElementById('table-body');
-
 	for (const photo of data.photos) {
 		const row = document.createElement('tr');
 		const tdId = document.createElement('td');
@@ -25,28 +27,45 @@ function setTableData(data) {
 		tdRoverName.textContent = photo.rover.name;
 		tdCamera.textContent = photo.camera.name;
 		moreButton.textContent = 'More';
+		moreButton.addEventListener('click', () => setPhotoData(photo));
 		tdDetails.appendChild(moreButton);
 
-		row.appendChild(idTd);
-		row.appendChild(idRoverName);
-		row.appendChild(idCamera);
-		row.appendChild(idDetails);
+		row.appendChild(tdId);
+		row.appendChild(tdRoverName);
+		row.appendChild(tdCamera);
+		row.appendChild(tdDetails);
 
 		tableBody.appendChild(row);
 	}
 }
 
 function setPhotoData(photo) {
+	console.log(photo);
 	image.src = photo.img_src;
+	earthP.textContent += photo.earth_date;
+	martianP.textContent += photo.rover.landing_date;
+	idP.textContent += photo.id;
+}
+
+function searchByDate() {
+	const dateFilter = document.getElementById('date-input').value;
+
+	if (dateFilter) {
+		date = dateFilter;
+		tableBody.innerHTML = '';
+		loadData();
+	}
 }
 
 function nextPage() {
+	tableBody.innerHTML = '';
 	page++;
 	loadData();
 }
 
 function previousPage() {
 	if (page > 1) {
+		tableBody.innerHTML = '';
 		page--;
 		loadData();
 	}
